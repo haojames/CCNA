de1b2ec18bb66711ae4b4df75f0d71ecbe5cab5b
Trần Anh Hào <85891846+haojames@users.noreply.github.com>
2026-04-18 22:54:53 +0700

    Create List.c

---
+++ List.c
@@ -0,0 +1,17 @@
+Thứ tự	Tên file	Ghi chú
+1	Switching.md	Nền tảng
+2	Inter_VLAN	Phân chia mạng
+3	STP.c	Chống loop
+4	2Etherchannel.c	Gom băng thông
+5	Router step.md	Cấu hình Router
+6	Wild card mask.md	Nhập môn subnet
+7	Admin distance.md	Lý thuyết route
+8	IGP.md + IGP_2	OSPF động
+9	Route Summarization...	Tối ưu
+10	ABR ASBR.md	Nâng cao OSPF
+11	Recovery.c	Cứu hộ Router
+12	First Hop Redundancy P...	Dự phòng
+13	Layer 2 Security (Port Se...	Bảo mật Switch
+14	ACL.c	Bảo mật luồng
+15	NAT.c	Kết nối Internet
+16	WAN.c	Mạng diện rộng
